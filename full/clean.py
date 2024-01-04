4c4cc514ce8d3116c933d82e2e84df6e242c6758
yunjiee <gigi9001!gmail.com>
2024-01-04 22:18:27 +0800

    Your commit message

---
+++ full/clean.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import re
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+
+'''
+Q1.資料有些都是同篇作者的文章
+Q2.資料要設置字數小於50字的刪除
+Q3.##沒有反映資料原始的樣子=>因為有刪掉一些奇怪的
+###刪掉那些咚咚，可以說一下
+'''
+
+# MBTI类型列表
+mbti_types = ["infp", "infj", "intj", "intp", "isfp", "isfj", "istj", "istp", "enfp", "enfj", "entj", "entp", "esfp", "esfj", "estj", "estp"]
+all_data = pd.DataFrame()
+# 预处理函数
+def preprocess_text(text):
+    text = str(text).lower()  # 确保文本是字符串类型并转换为小写
+    text = re.sub(r"\\t", "  ", text)
+    text = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", text)
+    text = re.sub(r"\'s", " \'s", text) 
+    text = re.sub(r"\'ve", " \'ve", text) 
+    text = re.sub(r"n\'t", " n\'t", text) 
+    text = re.sub(r"\'re", " \'re", text) 
+    text = re.sub(r"\'d", " \'d", text) 
+    text = re.sub(r"\'ll", " \'ll", text) 
+    text = re.sub(r",", " , ", text) 
+    text = re.sub(r"!", " ! ", text) 
+    text = re.sub(r"\(", " \( ", text) 
+    text = re.sub(r"\)", " \) ", text) 
+    text = re.sub(r"\?", " \? ", text)
+    text = re.sub(r'\s+', ' ', text)
+    text = re.sub(r"\s{2,}", " ", text) #去除多餘空
+    text = re.sub(r'http\S+', ' ', text)  # 移除URLs
+    text = re.sub(r'[^a-z .]', ' ', text)  # 替换非英文字符为一个空格
+    text = re.sub(r'[^a-z0-9 .,!?\'\"]', " ", text)  # 保留字母和标点符号，替换其他字符为一个空格
+    # 构建正则表达式，使用 | 分隔不要的词语，并添加括号来捕获匹配项
+    pattern = r'(' + '|'.join(map(re.escape, mbti_types)) + ')'
+    # 将匹配到的词语替换为 <type>
+    text = re.sub(pattern, '<type>', text)
+    return text
+
+for mbti_type in mbti_types:
+    file_name = f'./MBTI_project/data_personality/{mbti_type}_posts_data.csv'
+    data = pd.read_csv(file_name)
+    data = data.dropna(subset=['Content'])
+    data = data[data['Content'].str.strip().astype(bool)]  # 然后删除内容仅包含空格的行
+
+    data['processed_content'] = data['Content'].astype(str).apply(preprocess_text)
+    data['type'] = mbti_type
+
+    data = data[['type', 'processed_content']]
+    all_data = pd.concat([all_data, data], ignore_index=True)
+    
+all_data.to_csv('./MBTI_project/data_personality/processed_all_posts_data.csv', index=False)
+
+print("All processed data saved to: ./MBTI_project/data_personality/processed_all_posts_data.csv")
---
+++ full/eda.py
@@ -0,0 +1,107 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+############## Q:為甚麼資料會蠻一致的 =>需要想回答  ####################
+'''
+資料呈現方式:
+1.全部類型一起的分布圖(content文章的數量)
+2.四個類型的各自資料量(content文章的數量)
+3.跑出全部資料的content和number
+4.
+
+Q1.顏色要另外下載套組，讓他全部分色
+Q2.是否要以number => 如果以單詞數量呈現的話，就要統整出各類別最常用的單詞會更好
+
+'''
+##################################
+#收集到的全部資料
+file_path = 'C:/Users/student/yunjiee-python/MBTI_project/data_personality/processed_all_posts_data.csv'
+data = pd.read_csv(file_path)
+
+####
+
+def plot_mbti_distribution(data):
+    #設圖形大小
+    plt.figure(figsize=(40, 20))
+    # 设置 x 軸
+    plt.xticks(fontsize=16, rotation=0)
+    # 设置 y 軸
+    plt.yticks(fontsize=16, rotation=0)
+    # 使用 Matplotlib 的 bar 
+    type_counts = data['type'].value_counts()#每個類型不同的數量
+    #colors = [plt.cm.viridis(np.random.random()) for _ in range(len(type_counts))] #每個類型隨機對一個顏色
+    unique_colors = set()
+    colors = []
+    for _ in range(len(type_counts)):
+        while True:
+            random_color = plt.cm.viridis(np.random.random())
+            if random_color != unique_colors:
+                unique_colors.add(random_color)
+                colors.append(random_color)
+                break
+    plt.bar(type_counts.index, type_counts, color=colors)
+    # 设置 x 轴標籤
+    plt.xlabel('type',fontsize=16)
+    plt.ylabel('content', fontsize=16)
+    # 顯示圖形
+    plt.show()
+
+############## 四個類別的分佈圖 ##############
+from sklearn.preprocessing import LabelEncoder
+unique_type_list = ["infp","infj","intj","intp","isfp","isfj","istj","istp","enfp","enfj","entj","entp","esfp","esfj","estj","estp"]
+lab_encoder = LabelEncoder().fit(unique_type_list)
+#print(lab_encoder.transform(['INFJ']))
+
+def plot_mbti_dimensions(data):
+    #创建了四个新的特征列 ie、ns、ft 和 pj，这些特征代表了性格类型的不同维度（I/E、N/S、F/T、P/J）
+    data['ie'] = data['type'].apply(lambda t: 'i' if 'i' in t else 'e')
+    data['ns'] = data['type'].apply(lambda t: 'n' if 'n' in t else 's')
+    data['ft'] = data['type'].apply(lambda t: 'f' if 'f' in t else 't')
+    data['pj'] = data['type'].apply(lambda t: 'p' if 'p' in t else 'j')
+
+    # 打印每个维度的值计数
+    print(data['ie'].value_counts(), end='\n\n')
+    print(data['ns'].value_counts(), end='\n\n')
+    print(data['ft'].value_counts(), end='\n\n')
+    print(data['pj'].value_counts(), end='\n\n')
+
+    # 绘制直方图
+    plt.figure()
+    data['ie'].hist(); plt.show()
+    data['ns'].hist(); plt.show()
+    data['ft'].hist(); plt.show()
+    data['pj'].hist(); plt.show()
+
+    ###########說不定可以把照片全部一起出來
+    ##看看要不要以字數的方式計算
+    #是以content文本的方式計算
+
+import csv
+
+#跑出全部資料的content和number的數量
+def getstat(file_path):
+    lines = []
+    with open(file_path, "r", encoding="utf-8") as f:
+        reader = csv.reader(f)
+        for line in reader:
+            if len(line) == 2:  # 假设您的 CSV 文件有两列
+                lines.append(line[1])
+
+    print("Number of lines: {}".format(len(lines)))
+
+    wordcnt = 0
+    for line in lines:
+        words = line.split()  # 正确地分词每行文本
+        wordcnt += len(words)
+
+    print("Number of words: {}".format(wordcnt))
+
+################### 執行def ##################
+getstat(file_path)
+    
+# 绘制所有类型的分布图
+plot_mbti_distribution(data)
+
+# 绘制四个维度的分布图
+plot_mbti_dimensions(data)
