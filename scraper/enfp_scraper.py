6150edcbb6b241c335f5005070aec7b6e43ac515
xian108 <shannon96337@gmail.com>
2024-01-07 21:32:26 +0800

    crawler 500 data

--- scraper/enfp_scraper.py
+++ scraper/enfp_scraper.py
@@ -1,125 +1,3 @@
-# import pandas as pd
-# from bs4 import BeautifulSoup
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-# from selenium.common.exceptions import TimeoutException
-
-# # 定義在外面，保持相同的狀態
-# authors_set = set()
-
-# def crawl_enfp_forum(base_url, author_limit):
-#     driver = webdriver.Chrome()
-#     driver.implicitly_wait(60)
-
-#     posts_data = []
-
-#     try:
-#         # 初始頁面
-#         url = base_url
-#         driver.get(url)
-#         WebDriverWait(driver, 30).until(
-#             EC.presence_of_element_located((By.CLASS_NAME, 'structItem-title'))
-#         )
-
-#         soup = BeautifulSoup(driver.page_source, 'html.parser')
-
-#         # 找到qid為"page-nav-other-page"的元素
-#         page_nav_element = soup.select_one('[qid="page-nav-other-page"]')
-
-#         # 從中獲取頁數
-#         total_pages = int(page_nav_element.text)
-#         print(total_pages)
-
-#         for page in range(1, total_pages + 1):
-#             # 動態生成網址
-#             if page == 1:
-#                 url = base_url
-#             else:
-#                 url = f"{base_url}/page-{page}?sorting=latest-activity"
-
-#             driver.get(url)
-#             WebDriverWait(driver, 30).until(
-#                 EC.presence_of_element_located((By.CLASS_NAME, 'structItem-title'))
-#             )
-
-#             soup = BeautifulSoup(driver.page_source, 'html.parser')
-#             thread_links = soup.find_all('a', class_='thread-title--gtm')
-
-#             for i, thread_link in enumerate(thread_links):
-#                 thread_url_relative = thread_link['href']
-#                 thread_url = f"https://www.personalitycafe.com/{thread_url_relative}"
-
-#                 try:
-#                     driver.get(thread_url)
-#                     WebDriverWait(driver, 30).until(
-#                         EC.presence_of_element_located((By.CLASS_NAME, 'message'))
-#                     )
-#                     thread_posts_soup = BeautifulSoup(driver.page_source, 'html.parser')
-#                     post_content_element = thread_posts_soup.find('div', class_='bbWrapper')
-#                     post_content = post_content_element.text.strip() if post_content_element else ''
-
-#                     # 獲取作者名稱
-#                     author_element = thread_posts_soup.find('a', class_='MessageCard__user-info__name')
-#                     author_name = author_element.text if author_element else ''
-
-#                     print(f"Processing post from {author_name} ({thread_url})")
-
-#                     # 檢查是否已經爬取過該作者的貼文
-#                     if author_name not in authors_set:
-#                         authors_set.add(author_name)
-
-#                         # 處理該作者的貼文內容
-#                         posts_data.append({
-#                             'Author': author_name,
-#                             'Content': post_content,
-#                             'Thread_URL': thread_url
-#                         })
-
-#                     # 如果已經達到指定作者數目，則退出迴圈
-#                     if len(authors_set) >= author_limit:
-#                         break
-#                 except TimeoutException:
-#                     print(f"TimeoutException: Timed out on thread {i + 1}. Skipping...")
-#                     pass
-
-#             # 如果已經達到指定作者數目，則退出迴圈
-#             if len(authors_set) >= author_limit:
-#                 break
-#             # 模擬滾動以加載更多帖子，這部分可視情況選擇是否需要
-#             driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-#     except Exception as e:
-#         print(f"Error: {e}")
-
-#     finally:
-#         driver.quit()
-
-#     return posts_data
-
-# # 設定論壇基本網址
-# base_forum_url = "https://www.personalitycafe.com/forums/enfp-forum-the-inspirers.19"
-
-# # 設定要爬取的作者數目
-# author_limit = 400
-
-# # 爬取資料
-# all_posts_data = crawl_enfp_forum(base_forum_url, author_limit)
-
-# # 將所有的資料轉換成 DataFrame
-# df_all_posts = pd.DataFrame(all_posts_data)
-
-# # 檢查爬取到的作者數量是否足夠
-# if len(authors_set) < author_limit:
-#     print(f"Warning: Only crawled {len(authors_set)} unique authors, which is less than the required {author_limit}. The data may not be sufficient.")
-
-# # 設定 CSV 檔案路徑
-# csv_file_path = "enfp_posts_data_dynamic_url.csv"
-
-# # 將資料寫入 CSV 檔
-# df_all_posts.to_csv(csv_file_path, index=False)
-
 import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -140,7 +18,7 @@ def crawl_enfp_forum(base_url, author_limit, output_path="enfp_posts_data.csv"):
         # 初始頁面
         url = base_url
         driver.get(url)
-        WebDriverWait(driver, 30).until(
+        WebDriverWait(driver, 60).until(
             EC.presence_of_element_located((By.CLASS_NAME, 'structItem-title'))
         )
 
@@ -161,7 +39,7 @@ def crawl_enfp_forum(base_url, author_limit, output_path="enfp_posts_data.csv"):
                 url = f"{base_url}/page-{page}?sorting=latest-activity"
 
             driver.get(url)
-            WebDriverWait(driver, 30).until(
+            WebDriverWait(driver, 60).until(
                 EC.presence_of_element_located((By.CLASS_NAME, 'structItem-title'))
             )
 
@@ -174,7 +52,7 @@ def crawl_enfp_forum(base_url, author_limit, output_path="enfp_posts_data.csv"):
 
                 try:
                     driver.get(thread_url)
-                    WebDriverWait(driver, 30).until(
+                    WebDriverWait(driver, 120).until(
                         EC.presence_of_element_located((By.CLASS_NAME, 'message'))
                     )
                     thread_posts_soup = BeautifulSoup(driver.page_source, 'html.parser')
@@ -185,6 +63,10 @@ def crawl_enfp_forum(base_url, author_limit, output_path="enfp_posts_data.csv"):
                     author_element = thread_posts_soup.find('a', class_='MessageCard__user-info__name')
                     author_name = author_element.text if author_element else ''
 
+                    # 獲取貼文標題
+                    thread_title_element = thread_posts_soup.find('h1', class_='MessageCard__thread-title')
+                    thread_title = thread_title_element.text.strip() if thread_title_element else ''
+
                     print(f"Processing post from {author_name} ({thread_url})")
 
                     # 檢查是否已經爬取過該作者的貼文
@@ -194,6 +76,7 @@ def crawl_enfp_forum(base_url, author_limit, output_path="enfp_posts_data.csv"):
                         # 處理該作者的貼文內容
                         all_posts_data.append({
                             'Author': author_name,
+                            'Thread_Title': thread_title,  # 加入貼文標題
                             'Content': post_content,
                             'Thread_URL': thread_url
                         })
@@ -228,10 +111,10 @@ def crawl_enfp_forum(base_url, author_limit, output_path="enfp_posts_data.csv"):
 base_forum_url = "https://www.personalitycafe.com/forums/enfp-forum-the-inspirers.19"
 
 # 設定要爬取的作者數目
-author_limit = 400
+author_limit = 500
 
 # 設定輸出檔案的路徑
-output_csv_path = "enfp_posts_data.csv"
+output_csv_path = "enfp_posts_data17.csv"
 
 # 爬取資料
 all_posts_data = crawl_enfp_forum(base_forum_url, author_limit, output_path=output_csv_path)
