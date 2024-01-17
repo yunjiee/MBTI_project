6c8cf2acdec5d054d83da5998b9d43bce5b34c0e
yunjiee <gigi9001@gmail.com>
2024-01-17 17:25:07 +0800

    2024/01/17

--- full/out.py
+++ full/out.py
@@ -1,47 +1,88 @@
-### 加載訓練好的模型
 from transformers import BertForSequenceClassification, BertTokenizer
-model_path = r"D:\project\MBTI_project\full\output"
-model = BertForSequenceClassification.from_pretrained(model_path)
-print(model)
-### 加載詞彙表
-tokenizer = BertTokenizer.from_pretrained(model_path)
-print(tokenizer)
-
-### 進行預測
 import torch
 import pandas as pd
-df = pd.read_csv("D:\project\MBTI_project\data_personality\enfj_data.csv")  # 替换为您 CSV 文件的实际路径
-texts = df['Content'].tolist()  # 替换 'text_column_name' 为您的文本所在列的列名
-
-## 假设 new_data 是一个包含新文本的列表
-'''
-# 这里需要先从CSV文件中读取数据，然后进行处理
-# 假设您已经将CSV中的文本载入到一个名为texts的列表中
-inputs = tokenizer(texts, padding=True, truncation=True, max_length=128, return_tensors="pt")
-#truncation=True 将过长的序列截断至max_length
-
-import sys
-sys.path.append('/content/drive/My Drive/full/')  # 更新为您的路径
-'''
-from clean import preprocess_text
-
-# 预处理文本
-texts = [preprocess_text(text) for text in texts]
-##對模型進行預處理
-inputs = tokenizer(texts, padding=True, truncation=True, max_length=512, return_tensors="pt")
-#tokenizer 用于将文本转换为模型可以理解的格式
-print(inputs)
-#錯誤:　要求截断至 max_length，但未提供最大长度，且模型没有预定义的最大长度。默认为不截断
-##使用模型進行預測
-#torch.no_grad() 表示在此过程中不计算梯度
-with torch.no_grad():
-    outputs = model(**inputs)
-logits = outputs.logits
-print(logits)
+import re
 label_list = ["infp", "infj", "intj", "intp", "isfp", "isfj", "istj", "istp", "enfp", "enfj", "entj", "entp", "esfp", "esfj", "estj", "estp"]
-id_to_label = {i: label for i, label in enumerate(label_list)}
 
-predictions = torch.argmax(logits, dim=-1) #torch.argmax(logits, dim=-1)选出每个输入文本最可能的类别
-predictions = [id_to_label[id] for id in predictions.numpy()]
+def preprocess_text(text):
+    text = str(text).lower()  # 确保文本是字符串类型并转换为小写
+    text = re.sub(r'http\S+', ' ', text)  # 移除URLs
+    text = re.sub(r"\\t", "  ", text)
+    # 构建正则表达式，使用 | 分隔不要的词语，并添加括号来捕获匹配项
+    pattern = r'(' + '|'.join(map(re.escape, label_list)) + ')'
+    text = re.sub(r"\s{2,}", " ", text) #去除多餘空
+    text = re.sub(r'\s+', ' ', text) # 合并多个空格为一个空格
+    #print("111111clean11111",text)
+    text = re.sub(r'[^a-zA-Z\s\.\,\!\?\(\)\']', '', text)
+    # 将匹配到的词语替换为 <type>
+    text = re.sub(pattern, 'type', text)
+    text = re.sub(r"\'s", " \'s", text) 
+    text = re.sub(r"\'ve", " \'ve", text) 
+    text = re.sub(r"\'t", " \'t", text) 
+    text = re.sub(r"\'re", " \'re", text) 
+    text = re.sub(r"\'d", " \'d", text) 
+    text = re.sub(r"\'ll", " \'ll", text) 
+    text = re.sub(r", ", " , ", text)
+    #print("11111111111111111111111",text)
+    text = re.sub(r"\. ", " . ", text) 
+    #print("22222222222222222222222",text)
+    text = re.sub(r"'", " ' ", text)  
+    text = re.sub(r"!", " ! ", text) 
+    text = re.sub(r"\(", " ( ", text) 
+    text = re.sub(r"\)", " ) ", text) 
+    text = re.sub(r"\? ", " ? ", text)
+    #print("22222222222222222222222",text)
+    text = re.sub(r"\.{2,}", ".", text)
+    text = re.sub(r"\?{2,}", " ? ", text)
+    text = re.sub(r"\s{2,}", "", text) #去除多餘空
+    text = re.sub(r"\!{2,}", " ! ", text) #去除多餘空
+    #print("33333333333333333333333",text)
+    return text
 
-print(predictions)
+def load_model_and_tokenizer(model_path):
+    model = BertForSequenceClassification.from_pretrained(model_path)
+    tokenizer = BertTokenizer.from_pretrained(model_path)
+    return model, tokenizer
+
+def preprocess_csv(csv_path, text_column_name):
+    df = pd.read_csv(csv_path, encoding='iso-8859-1')
+    texts = df[text_column_name].tolist()
+    # 预处理文本
+    texts = [preprocess_text(text) for text in texts]
+    return texts
+
+def predict_text(model, tokenizer, texts):
+    ##對模型進行預處理
+    inputs = tokenizer(texts, padding=True, truncation=True, max_length=512, return_tensors="pt")
+
+    with torch.no_grad():
+        outputs = model(**inputs)
+    logits = outputs.logits
+    id_to_label = {i: label for i, label in enumerate(label_list)}
+
+    predictions = torch.argmax(logits, dim=-1)
+    predictions = [id_to_label[id] for id in predictions.numpy()]
+
+    return predictions
+'''
+if __name__ == "__main__":
+    model_path = f"D:/project/MBTI_project/full/output/"  # 替换为您的模型路径
+    model, tokenizer = load_model_and_tokenizer(model_path)
+
+    csv_file_path = "D:/project/MBTI_project/full/data/dev.csv"  # 替换为您的CSV文件路径
+    column_name = "Content"  # 替换为文本所在列的列名
+    texts = preprocess_csv(csv_file_path, column_name)
+    predictions = predict_text(model, tokenizer, texts)
+    print(predictions)
+'''
+if __name__ == "__main__":
+    model_path = f"D:/project/MBTI_project/full/output/"  # 替换为您的模型路径
+    model, tokenizer = load_model_and_tokenizer(model_path)
+    # 定义一个名为 text 的列表，如果它还不存在
+    texts = []
+    # 要添加的句子
+    sentence = "anyone ever have something happen that you just have to share with someone or have a random thought or idea that you feel like putting out there    or do you simply feel like rambling or ruminating    well   here  s a place to make it known if there is n t a thread for it   go for it here  anything and everything is welcome and encouraged "
+    # 将句子添加到列表中
+    texts = preprocess_text(texts.append(sentence))
+    predictions = predict_text(model, tokenizer, texts)
+    print(predictions)
