b03eccf3d2b053d97d9db55b253428d47a9110a6
yunjiee <gigi9001@gmail.com>
2024-09-04 16:45:48 +0800

    9.4

--- full/dataloader.py
+++ full/dataloader.py
@@ -3,7 +3,6 @@ from torch.utils.data import (DistributedSampler, DataLoader, RandomSampler, Seq
 ### dataloader 數據加載器 =>基于 PyTorch 的模型准备训练和评估数据。它将数据转换为模型所需的格式 ###
 #PyTorch 中用于加载数据的一个工具，可以为模型训练提供批量数据
 #预处理的数据转换成 PyTorch DataLoader 的形式，这是为了让数据能够被 PyTorch 模型有效地使用
-
 #訓練
 def get_train_dataloader(args, train_features):
     #args：包含训练配置的参数 
@@ -12,13 +11,9 @@ def get_train_dataloader(args, train_features):
     all_input_mask = torch.tensor([f.input_mask for f in train_features], dtype=torch.long)
     all_segment_ids = torch.tensor([f.segment_ids for f in train_features], dtype=torch.long)
     all_label_ids = torch.tensor([f.label_id for f in train_features], dtype=torch.long)
-    library(writexl)
-    # 将数据框写入 Excel 文件
-    # write_xlsx(train_data_df, "train_data.xlsx")
-
     #創建數據庫    
     train_data = TensorDataset(all_input_ids, all_input_mask, all_segment_ids, all_label_ids)
-    print(train_data)
+    train_data.to_excel("D:/project/MBTI_project/data/train_data_ids.xlsx", index=False)
     #輸入張量和標籤和數據一起，方便模型訓練
     #採樣數據
     if args.local_rank == -1:
@@ -41,6 +36,7 @@ def get_eval_dataloader(args, eval_features):
     
     #評估 
     eval_data = TensorDataset(all_input_ids, all_input_mask, all_segment_ids, all_label_ids)
+    eval_data.to_excel("D:/project/MBTI_project/data/eval_data_ids.xlsx", index=False)
     # Run prediction for full data
     eval_sampler = SequentialSampler(eval_data)
     eval_dataloader = DataLoader(eval_data, sampler=eval_sampler, batch_size=args.eval_batch_size)
