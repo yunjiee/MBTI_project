f23d918e8d48965f64f6850cd0c1ca82ddcd7c14
yunjiee <gigi9001@gmail.com>
2024-09-06 21:42:39 +0800

    9.6

--- full/fine_tune_save_colab.py
+++ full/fine_tune_save_colab.py
@@ -1,5 +1,24 @@
 #%%writefile '/content/drive/My Drive/full/fine_tune_save_colab.py'
+# coding=utf-8
+# Copyright 2018 The Google AI Language Team Authors and The HuggingFace Inc. team.
+# Copyright (c) 2018, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 ###############"""BERT finetuning runner.微調"""########################
+#理想狀態來說: 應該只要給他下面兩個資料data_dir和output_dir，就可以先跑跑看
+#parser.add_argument("--data_dir", default="./", type=str)
+#parser.add_argument("--output_dir", default="./output/", type=str)
+#=>都測試好後,再來看base-large的部分
 from __future__ import absolute_import, division, print_function
 #用於确保代码在不同版本的Python中具有一致的行为(维护同时需要在Python 2和Python 3环境下运行的代码非常有用)
 
